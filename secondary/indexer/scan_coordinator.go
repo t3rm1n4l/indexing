0add5e6e28e5676e3a7bb2f6a35e35a51ae85c98
Sarath Lakshman <sarathlakshman@slynux.com>
2015-04-28 10:12:46 +0530

    MB-14637 scanner: Use lazy debug logger for logging timestamps
    Change-Id: I9746590941b9e09831b1ffb9082064447cbd532e

--- secondary/indexer/scan_coordinator.go
+++ secondary/indexer/scan_coordinator.go
@@ -550,9 +550,12 @@ func (s *scanCoordinator) serverCallback(protoReq interface{}, conn net.Conn,
 	defer func() { s.handleError(req.LogPrefix, w.Done()) }()
 
 	logging.Infof("%s REQUEST %s", req.LogPrefix, req)
+
 	if req.Consistency != nil {
-		logging.Debugf("%s requested timestamp: %s => %s", req.LogPrefix,
-			strings.ToLower(req.Consistency.String()), ScanTStoString(req.Ts))
+		logging.LazyDebug(func() string {
+			return fmt.Sprintf("%s requested timestamp: %s => %s", req.LogPrefix,
+				strings.ToLower(req.Consistency.String()), ScanTStoString(req.Ts))
+		})
 	}
 
 	if s.tryRespondWithError(w, req, err) {
@@ -569,7 +572,10 @@ func (s *scanCoordinator) serverCallback(protoReq interface{}, conn net.Conn,
 
 	defer DestroyIndexSnapshot(is)
 
-	logging.Infof("%s snapshot timestamp: %s", req.LogPrefix, ScanTStoString(is.Timestamp()))
+	logging.LazyDebug(func() string {
+		return fmt.Sprintf("%s snapshot timestamp: %s",
+			req.LogPrefix, ScanTStoString(is.Timestamp()))
+	})
 	s.processRequest(req, w, is, t0)
 }
 
